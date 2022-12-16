4df2cf0345a5e7329546da04685cb25acf5cb1a4
nikamunjishvili <nikamunja31415@gmail.com>
2022-12-16 13:51:36 +0400

    well,Finished Project

--- src/App.js
+++ src/App.js
@@ -12,10 +12,9 @@ function App() {
         <Router>
           <Header />
             <Routes>
-                <Route index element={<Home />}></Route>
+                <Route index path='/' element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="/*" element={<h1>Error Page</h1>}></Route>
             </Routes>
         </Router>
       </HashRouter>
